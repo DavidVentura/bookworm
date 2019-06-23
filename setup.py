d4251e6ae8b244ece79e23f0c77d85adbaeeb91e
david <davidventura27@gmail.com>
2019-06-23 19:17:31 +0200

    add entrypoints

--- setup.py
+++ setup.py
@@ -3,4 +3,12 @@ setup(
     name="bookworm",
     version="0.1",
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'file_fetcher = bookworm.file_fetcher:main',
+            'ircclient = bookworm.ircclient:main',
+            'unpacker = bookworm.unpacker:main',
+            'web = bookworm.web:main',
+            ]
+        }
 )
