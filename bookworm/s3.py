948cb411e318cd8cc99db3e4f08987afd3390ad7
david <davidventura27@gmail.com>
2019-10-06 22:40:51 +0200

    use a named profile for minio credentials

--- bookworm/s3.py
+++ bookworm/s3.py
@@ -3,11 +3,5 @@ import botocore
 
 def client():
     config = botocore.client.Config(connect_timeout=3, read_timeout=3, retries={'max_attempts': 0})
-    session = boto3.session.Session()
-    return session.client(
-        service_name='s3',
-        aws_access_key_id='LBSBQ46DB9S160XIW438',
-        aws_secret_access_key='7nVT+TwGzPoC02AIIY4+1fVNdj07RQt+ntd55S+I',
-        endpoint_url='http://localhost:9000',
-        config=config
-    )
+    session = boto3.session.Session(profile_name='bookworm')
+    return session.client(service_name='s3', endpoint_url='http://localhost:9000', config=config)
